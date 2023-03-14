1b465eb1f9e3578201942a10a67f73a999136d86
Breninho23 <brenodiasprotz@gmail.com>
2023-03-14 11:01:55 -0300

    Adicionando entidade usuario.

---
+++ src/main/java/hospital/vital/api/domain/usuario/Usuario.java
@@ -0,0 +1,23 @@
+package hospital.vital.api.domain.usuario;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+/**
+ * @author Breno
+ */
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+
+}
